cccaa7055af63b24a2bcbcf8068e4022ed874cc7
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2022-09-17 18:58:26 +0700

    Fix linearMap dan pxlFreq

--- tools/linearMap.m
+++ tools/linearMap.m
@@ -1,5 +1,5 @@
 function out = linearMap(a, b)
-  out = b:a:b+255*a;
+  out = b + 0:255 .* a;
   out(out <= 0) = 0;
   out(out >= 255) = 255;
   out = cast(out, 'uint8');
--- tools/pxlFreq.m
+++ tools/pxlFreq.m
@@ -1,11 +1,11 @@
 function ret = pxlFreq(image)
   if (ndims(image) == 3)
-    ret = zeros(1,256,3);
+    ret = zeros(3,256);
     % Optimize using sum()
     for b = 1:256
       temp = (image >= b) & (image < (b + 1));
       temp = reshape(temp, 1, [], size(image, 3));
-      ret(1,b,:) = sum(temp, 2);
+      ret(:,b) = sum(temp, 2);
     end
   else
     ret = zeros(1,256);

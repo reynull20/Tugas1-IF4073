b6b9eb7c76fb181cb29fe49354cc5bfd60cd9fb9
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2022-09-19 14:45:50 +0700

    Add extending histogram for specifyHistogram

--- tools/specifyHistogram.m
+++ tools/specifyHistogram.m
@@ -3,6 +3,11 @@ function out = specifyHistogram (h, g)
   h_s = h_s ./ h_s(:,size(h_s,2));
   g_s = cumsum(g, 2);
   g_s = g_s ./ g_s(:,size(g_s,2));
+  if (size(h_s, 1) < size(g_s, 1))
+    h_s = repmat(h_s, size(g_s, 1), size(h_s, 2));
+  elseif (size(h_s, 1) > size(g_s, 1))
+    g_s = repmat(g_s, size(h_s, 1), size(g_s, 2));
+  end
   out = zeros(size(h_s));
   for i = 1:size(h_s,1)
     out(i,:) = lookup(g_s(i,:), h_s(i,:), 'r');
